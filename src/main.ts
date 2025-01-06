cd496d2b494f456cb8351845c6ec9d1fb93086f0
oceeboy <osieta33@gmail.com>
2025-01-05 20:25:01 -0500

    prod test5

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.setGlobalPrefix('api'); to be added before deploying after test
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();
