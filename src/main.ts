4733eb8a114294c9476cf99204e02e62b4973329
oceeboy <osieta33@gmail.com>
2025-01-05 20:22:36 -0500

    prod test4

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-
-  // Enable CORS
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://your-frontend-domain.com'], // Allow specific origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Authorization', 'Content-Type'],
-    credentials: true,
-  });
-
-  await app.listen(3001, '0.0.0.0'); // Ensure it listens on all network interfaces
+  const app = await NestFactory.create(AppModule);
+  // app.setGlobalPrefix('api'); to be added before deploying after test
+  await app.listen(3001);
 }
 bootstrap();
