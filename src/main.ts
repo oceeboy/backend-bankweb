8195d6577013e37b9be4e0fce80c936a7bf42f59
oceeboy <osieta33@gmail.com>
2025-01-09 17:39:05 -0500

    testing cors fixes

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
+// import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.use(
-    cors({
-      origin: '*', // Allow multiple origins
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-      credentials: true,
-    }),
-  );
+  app.enableCors({
+    origin: '*',
+  });
 
   await app.listen(3001);
 }
