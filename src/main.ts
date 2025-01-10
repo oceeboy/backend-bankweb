dc1e1c3272ecc7610724ed4ad839cc1876f23bac
oceeboy <osieta33@gmail.com>
2025-01-10 05:58:21 -0500

    testing cors fixes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 // import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix('api');
   app.enableCors({
